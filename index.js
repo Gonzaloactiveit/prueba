f0507314aa547d9d32891b67f2a055ad5329dc25
Gonzalo Ibarra <gibarra@activeit.cl>
2020-06-10 12:59:44 -0400

    agregada la conexion a base de datos y actualizado el metodo POST

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require('express')
 const bodyparser = require('body-parser')
+const mongoose = require('mongoose')
+
+const Product = require('./models/product.js')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -20,9 +23,21 @@ app.get('/api/product/:productId',(req,res) =>{
 })
 
 app.post('/api/product', (req,res) => {
+    console.log('POST /api/product')
     console.log(req.body)
-    res.send(200, {message:'El producto se ha recibido'})
 
+    let product = new Product()
+    product.name = req.body.name
+    product.picture = req.body.picture
+    product.price = req.body.price
+    product.category =req.body.category
+    product.description = req.body.description;
+    
+    product.save((err, productStored) =>{
+        if(err) res.status(500).send({message: `Error al salvar la base de datos: ${err}`})
+
+        res.status(200).send({product: productStored})
+    })
 })
 
 app.put('/api/product/:productId', (req,res) => {
@@ -35,6 +50,11 @@ app.delete('/api/product/:productId', (req,res) => {
 
 })
 
-app.listen(port, () => {
-    console.log(`API REST corriendo en http://localhost:${port}`)
+mongoose.connect('mongodb+srv://gibarra:Glassgon1292@cluster0-agvp6.mongodb.net/shop?retryWrites=true&w=majority', (err,res) =>{
+    if (err) throw err
+    console.log('Conexion a la base de datos establecido...')
+
+    app.listen(port, () => {
+        console.log(`API REST corriendo en http://localhost:${port}`)
+    })
 })
