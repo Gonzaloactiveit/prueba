551c780f9dd7c50a003ddf4b49906a18d44df59d
Gonzalo Ibarra <gibarra@activeit.cl>
2020-06-10 16:37:20 -0400

    Agregado modelo usuario y autentificador

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb+srv://gibarra:Glassgon1292@cluster0-agvp6.mongodb.net/shop?retryWrites=true&w=majority'
+    db: process.env.MONGODB || 'mongodb+srv://gibarra:Glassgon1292@cluster0-agvp6.mongodb.net/shop?retryWrites=true&w=majority',
+    SECRET_TOKEN: 'miclavetoken'
 }
